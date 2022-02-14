8c327bd2ce60185e0565e5c6f9afff385a4ad765
Izzathajar <muhammadizzat333@gmail.com>
2022-02-14 12:42:37 +0800

    correction for map marking

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -53,7 +53,7 @@ body { margin: 0; padding: 0; }
                 },
                 'geometry': {
                     'type': 'Point',
-                    'coordinates': [latitude, longitude]
+                    'coordinates': [longitude, latitude]
                 }
             };
     }
